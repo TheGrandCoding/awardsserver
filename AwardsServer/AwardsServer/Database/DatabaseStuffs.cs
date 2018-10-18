aa67877692a99c6c82eeb6949807d841011b23bb
Alex Chester <cheale14@coundoncourt.org>
2018-10-18 13:29:04 +0100

    Capitalised things
    Closes #21

--- AwardsServer/AwardsServer/Database/DatabaseStuffs.cs
+++ AwardsServer/AwardsServer/Database/DatabaseStuffs.cs
@@ -158,7 +158,7 @@ namespace AwardsServer
                         Logging.Log(Logging.LogSeverity.Warning, "Database table for category " + cat.ID + " missing, attempting to create..");
                         OleDbCommand tableCommand = new OleDbCommand();
                         tableCommand.Connection = connection;
-                        tableCommand.CommandText = $"create table Category{cat.ID} (UserName varchar(255), VotedFor varchar(255), TimeVoted varchar(103));";
+                        tableCommand.CommandText = $"CREATE TABLE Category{cat.ID} (UserName varchar(255), VotedFor varchar(255), TimeVoted varchar(103));";
                         tableCommand.ExecuteNonQuery();
                         reader2 = command2.ExecuteReader();
                     }
@@ -222,7 +222,7 @@ namespace AwardsServer
             {
                 throw new ArgumentException("Unknown category id: " + categoryID.ToString(), "categoryID");
             }
-            ExecuteCommand($"insert into Category{category.ID} (UserName , VotedFor, TimeVoted) values ('{votedBy.AccountName}','{voted.AccountName}','{DateTime.Now}')");
+            ExecuteCommand($"INSERT INTO Category{category.ID} (UserName , VotedFor, TimeVoted) VALUES ('{votedBy.AccountName}','{voted.AccountName}','{DateTime.Now}')");
         }
     }
 }

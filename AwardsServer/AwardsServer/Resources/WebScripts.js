033b718c96cfb82bb48530ead9107c726219a511
CheAle14 <thedark1513@gmail.com>
2019-03-23 08:17:14 +0000

    Vote dumping, website improvements
    `save votes` now writes to RawVotes.log
    Website has had improvements: Button to change student you are viewing.

--- AwardsServer/AwardsServer/Resources/WebScripts.js
+++ AwardsServer/AwardsServer/Resources/WebScripts.js
@@ -1,9 +1,28 @@
-// currently does nothing
-function terminateAll() {
-    var hiddenDocs = document.getElementsByClassName("hidden");
-    for (var i = 0; i < hiddenDocs.length; ++i) {
-        var item = hiddenDocs[i];
-        item.innerHTML = '[Removed]';
+// global javascript things
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
     }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
-terminateAll();
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {
+    document.cookie = name + '=; Max-Age=-99999999;';
+}
+
+function resetStudent() {
+    eraseCookie('View');
+    window.location.reload(true); // true = require get from server
+}
